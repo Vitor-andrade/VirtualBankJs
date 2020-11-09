1adc50416ef5df3146253fe918fd4f9a558b35b4
Vitor de Andrade Cavalcante <vitorandrade9@hotmail.com>
2020-11-09 14:23:12 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+
+// Todo atributo que foi criado, mas não definido, por padrão é undefined, mas ele pode ser Null, ao ser declarado explícitamente.
+const conta2 = new ContaCorrente(102, cliente2);
+
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+
+console.log(contaCorrenteRicardo);
+
+
